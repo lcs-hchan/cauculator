783c0f4d1c0816947b1474ffbd441a333347461a
Hayden Chan <hchan@lcs.on.ca>
2024-01-15 11:07:57 -0500

    Initial Commit

---
+++ cauculator/cauculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  cauculatorApp.swift
+//  cauculator
+//
+//  Created by Hayden Chan on 2024-01-15.
+//
+
+import SwiftUI
+
+@main
+struct cauculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
